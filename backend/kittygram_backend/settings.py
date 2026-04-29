0acb870fb39d1f70b8ffbf576bf3e953356082e8
Zero4411-op <safafakc0576@gmail.com>
2026-04-29 17:51:03 +0300

    Исправил по замечаниям ревью

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -104,7 +104,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -114,6 +114,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+    ],
 }
 
 STATIC_ROOT = BASE_DIR / 'collected_static'
